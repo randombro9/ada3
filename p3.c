128537c4decfaae4e5021770db386197764546bc
randombro9 <101424866+randombro9@users.noreply.github.com>
2022-06-30 00:30:26 +0530

    hdio

--- p3.c
+++ p3.c
@@ -1,21 +1,73 @@
+
 #include <stdio.h>
-#include<unistd.h>
-int main(int argc, char *argv[])
-{
-if(argc!=3)
-{
-printf("usage: %s <src_file><dest_file>\n",argv[0]);
-return 0;
+
+
+void swap(int *a, int *b) {
+int temp = *a;
+*a = *b;
+*b = temp;
 }
-if(link(argv[1],argv[2])==-1)
+
+void heapify(int arr[], int n, int i) {
+
+int largest = i;
+
+int left = 2 * i + 1;
+
+int right = 2 * i + 2;
+
+
+if (left < n && arr[left] > arr[largest])
+
+	largest = left;
+
+
+if (right < n && arr[right] > arr[largest])
+
+	largest = right;
+
+if (largest != i) {
+
+	swap(&arr[i], &arr[largest]);
+
+	heapify(arr, n, largest);
+
+}
+
+}
+
+void heapSort(int arr[], int n) {
+
+for (int i = n / 2 - 1; i >= 0; i--)
+
+	heapify(arr, n, i);
+
+for (int i = n - 1; i >= 0; i--) {
+
+	swap(&arr[0], &arr[i]);
+
+	heapify(arr, i, 0);
+
+}
+
+}
+
+void printArray(int arr[], int n)
 {
-printf("link error\n");
-return 1;
+for (int i = 0; i < n; i++)
+	printf("%d ", arr[i]);
+printf("\n");
+
 }
-printf("files linked\n");
-printf("Inode number of linked files\n");
-char str[100];
-sprintf(str,"ls –i %s %s \n",argv[1],argv[2]);
-system(str);
-return 0;
+
+
+int main()
+{
+int arr[] = {12, 11, 13, 5, 6, 7};
+int n = 6;
+heapSort(arr, n);
+printf("Sorted array is \n");
+printArray(arr, n);
+
 }
+
